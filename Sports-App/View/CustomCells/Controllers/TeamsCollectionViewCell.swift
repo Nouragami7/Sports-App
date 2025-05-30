ecee3949e9c11701483d276ddbe874ec528635ec
NourAgami <nuralquds123@gmail.com>
2025-05-30 22:39:50 +0300

    team cell

---
+++ Sports-App/View/CustomCells/Controllers/TeamsCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TeamsCollectionViewCell.swift
+//  Sports-App
+//
+//  Created by macOS on 30/05/2025.
+//
+
+import UIKit
+
+class TeamsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var teamName: UILabel!
+   
+    @IBOutlet weak var teamLogo: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
