83341bbf7da8410a96624149b777392d1e3f8e1a
NourAgami <nuralquds123@gmail.com>
2025-06-04 10:06:37 +0300

    make splash screen

--- Sports-App/View/Screens/Controllers/ViewController.swift
+++ Sports-App/View/Screens/Controllers/ViewController.swift
@@ -7,18 +7,43 @@
 
 import UIKit
 import Alamofire
+import Lottie
 
 class ViewController: UIViewController {
 
 
-    override func viewDidLoad() {
-        super.viewDidLoad()        
+    private var animationView: LottieAnimationView!
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .white
+            setupAnimation()
+        }
+        
+        private func setupAnimation() {
+            animationView = .init(name: "logo")
+            animationView.frame = view.bounds
+            animationView.contentMode = .scaleAspectFit
+            animationView.center = view.center
+            animationView.loopMode = .playOnce
+            animationView.animationSpeed = 1.0
+            
+            view.addSubview(animationView)
+            
+            animationView.play { [weak self] finished in
+                if finished {
+                    self?.navigateToHome()
+                }
+            }
+        }
+        
+        func navigateToHome() {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let teamDetailsVC = storyboard.instantiateViewController(withIdentifier: "homeScreen") as? HomeCollectionViewController {
+                self.navigationController?.pushViewController(teamDetailsVC, animated: true)
+            }
     }
 
-
-    @IBAction func navigation(_ sender: Any) {
-        let leaguesVC = LeaguesTableViewController(nibName: "LeaguesTableViewController", bundle: nil)
-            self.navigationController?.pushViewController(leaguesVC, animated: true)
-    }
 }
 
